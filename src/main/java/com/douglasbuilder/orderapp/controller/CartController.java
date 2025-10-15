ddff8ad923f6ee2185f66be86f9a27dbb490bacf
Segatto <78928583+DouglasVSegatto@users.noreply.github.com>
2025-10-15 10:35:34 -0600

    new quantity endpoint option, to later review

--- src/main/java/com/douglasbuilder/orderapp/controller/CartController.java
+++ src/main/java/com/douglasbuilder/orderapp/controller/CartController.java
@@ -38,16 +38,24 @@ public class CartController {
         return ResponseEntity.ok().build();
     }
 
-    @DeleteMapping("/items/{itemID}")
-    public ResponseEntity<?> deleteItem(@RequestParam Long userId, @PathVariable Long itemID) {
-        cartService.deleteItem(userId, itemID);
+    @DeleteMapping("/items/{itemId}")
+    public ResponseEntity<?> deleteItem(@RequestParam Long userId, @PathVariable Long itemId) {
+        cartService.deleteItem(userId, itemId);
         return ResponseEntity.ok().build();
     }
 
     //TODO - Validate negative numbers in DTO
-    @PutMapping("/items/{itemID}")
-    public ResponseEntity<?> updateItem(@RequestParam Long userId, @PathVariable Long itemID, @RequestBody UpdateCartItemQuantityDTO quantityDTO){
-        CartItem userCartItemUpdated = cartService.updateCartItem(userId, itemID, quantityDTO.getQuantity());
+    @PutMapping("/items/{itemId}")
+    public ResponseEntity<?> updateItem(@RequestParam Long userId, @PathVariable Long itemId, @RequestBody UpdateCartItemQuantityDTO quantityDTO){
+        CartItem userCartItemUpdated = cartService.updateCartItem(userId, itemId, quantityDTO.getQuantity());
+        return ResponseEntity.ok().body(cartMapper.toCartItemResponseDTO(userCartItemUpdated));
+    }
+
+    //TODO - Validate negative numbers in DTO
+    // duplicada com o decima, eu prefiro esse pois quantiydade e a unica coisa que a gente vai atualizar atraver do cartitem
+    @PutMapping("/items/{itemId}/quantity/{newQuantity}")
+    public ResponseEntity<?> updateItemQuantity(@RequestParam Long userId, @PathVariable Long itemId, Integer newQuantity){
+        CartItem userCartItemUpdated = cartService.updateCartItem(userId, itemId, newQuantity);
         return ResponseEntity.ok().body(cartMapper.toCartItemResponseDTO(userCartItemUpdated));
     }
 
